c7d41896d3d5f0086157f3ca1fdb2ca6b44eb710
IgorHamzi <igorwhamzi@gmail.com>
2022-01-31 19:53:11 -0300

    adiciona tela de explorar drinks e foods (70 a 73)

---
+++ src/pages/ExploreFoodsNationalities.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ExploreFoodsNationalities extends Component {
+  render() {
+    return <div>explore food nationatilies teste</div>;
+  }
+}
