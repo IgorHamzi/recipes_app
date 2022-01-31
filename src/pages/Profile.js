1507c5c7e41d9ba3700f767d2cf706befa769590
IgorHamzi <igorwhamzi@gmail.com>
2022-01-31 20:08:04 -0300

    adiciona tela de perfil (82,84,85 e 86)

---
+++ src/pages/Profile.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Explore extends Component {
+  render() {
+    return (
+      <div>
+        <div data-testid="profile-email">email</div>
+        <Link to="/done-recipes">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+          >
+            Logout
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
