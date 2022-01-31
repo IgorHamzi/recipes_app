45b39d3b6bcd5f24750e582d00c9a8b5dbe5cd6f
IgorHamzi <igorwhamzi@gmail.com>
2022-01-31 19:26:07 -0300

    tela de explorar (67,68 e 69)

---
+++ src/pages/ExploreDrinks.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ExploreDrinks extends Component {
+  render() {
+    return <div>Teste Drink</div>;
+  }
+}
---
+++ src/pages/ExploreFoods.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ExploreFoods extends Component {
+  render() {
+    return <div>Teste Food</div>;
+  }
+}
