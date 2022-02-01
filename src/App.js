0b8032360884198c5da36392d8986b42488f8026
Willian de Oliveira Marques <willianmarques_fsa@hotmail.com>
2022-02-01 14:37:25 -0300

    90%login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Routes from './routes';
+import FamilyContext from './Context/context';
 
 function App() {
+  const [senha, setSenha] = useState();
+  const [mail, setMail] = useState();
+
+  const funt = (m, s) => {
+    setSenha(s);
+    setMail(m);
+  };
+
+  const contextValue = {
+    user: {
+      mail,
+      senha,
+    },
+    funt,
+  };
+
   return (
     <div>
       <BrowserRouter>
-        <Routes />
+        <FamilyContext.Provider value={ contextValue }>
+          <Routes />
+        </FamilyContext.Provider>
       </BrowserRouter>
     </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
+import validator from 'validator';
+import FamilyContext from '../Context/context';
 
-export default class Login extends Component {
-  render() {
-    return <div>oi</div>;
-  }
+function Login() {
+  const [mail, setMail] = useState();
+  const [senha, setSenha] = useState('');
+  const { funt } = useContext(FamilyContext);
+
+  const setMaill = ({ target }) => {
+    setMail(target.value);
+  };
+
+  const setSenhaa = ({ target }) => {
+    setSenha(target.value);
+  };
+  const tres = 6;
+
+  return (
+    <div>
+      <label htmlFor="email">
+        <input
+          data-testid="email-input"
+          name="email"
+          type="text"
+          value={ mail }
+          onChange={ (e) => setMaill(e) }
+        />
+      </label>
+      <label htmlFor="senha">
+        <input
+          data-testid="password-input"
+          name="senha"
+          type="text"
+          value={ senha }
+          onChange={ (e) => setSenhaa(e) }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ senha.length < tres || !validator.isEmail(mail) }
+        onClick={ () => funt(mail, senha) }
+      >
+        Enter
+      </button>
+    </div>
+  );
 }
+
+export default Login;
