0cb8310c76fc8aec4059cca99810dbd0ddb6b8fc
Felipe David <felipe.besorean@hotmail.com>
2022-02-03 20:07:51 -0300

    requisitos tela de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,11 +4,13 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const email = JSON.parse(localStorage.getItem('user'));
+  console.log(email);
   return (
     <>
       <Header title="Profile" haveSearch={ false } />
       <div>
-        <div data-testid="profile-email">email</div>
+        <div data-testid="profile-email">{ email.email }</div>
         <Link to="/done-recipes">
           <button
             type="button"
@@ -25,14 +27,15 @@ function Profile() {
             Favorite Recipes
           </button>
         </Link>
-        <Link to="/">
-          <button
-            type="button"
-            data-testid="profile-logout-btn"
-          >
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+        >
+          <a href="/">
             Logout
-          </button>
-        </Link>
+          </a>
+        </button>
       </div>
       <Footer />
     </>
